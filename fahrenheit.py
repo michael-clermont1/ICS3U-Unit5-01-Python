65c39bac64baaa42f5b87de49c0533f33ee229a0
Michael Clermont <michael.clermont@mths.ca>
2022-05-02 18:19:50 +0000

    edited fahrenheit program

--- fahrenheit.py
+++ fahrenheit.py
@@ -9,15 +9,20 @@ def fahrenheit():
     # this function calculates the fahrenheit from celsius
 
     # input
-    celsius = float(input("Enter a temperature(°C): "))
+    celsius_string = input("Enter a temperature(°C): ")
 
-    # process
-    fahrenheit_float = (celsius * 1.8) + 32
+    # process & output
+    try:
+        celsius_float = float(celsius_string)
+        fahrenheit_float = (celsius_float * 1.8) + 32
+        print("")
+        print("{0}°C is equal to {1}°F".format(celsius_float, fahrenheit_float))
+    except Exception:
+        print("\nThat is not a decimal number.")
+    finally:
+        print("\nDone.")
 
-    # output
-    print("")
-    print("{0}°C is equal to {1}°F".format(celsius, fahrenheit_float))
-    print("\nDone.")
+
 
 def main():
     # this function just calls other functions
